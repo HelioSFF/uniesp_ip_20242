aeaf13bba20d87863440a4b3b307f39553f7a0bf
Hélio de Siqueira Figueiredo Filho <heliosiqueiraff@gmail.com>
2024-09-11 23:02:53 -0300

    commit

--- aula05/lista_de_exercicios/questao_15.py
+++ aula05/lista_de_exercicios/questao_15.py
@@ -1,41 +1,30 @@
-from datetime import datetime
-
-def viagem_no_tempo(ener,localx,localy,time):
+def viagem_no_tempo(ener,localx,localy,date):
     if ener >= 80:
         print(f'Energia de {ener}%')
         print(f'Energia checada!')
-        return True
+
+        if type(localx) == float or int and type(localy) == float or int:
+            print(f'Coordenadas detectadas x:{localx} y: {localy}')
+
+            if type(date) == int:
+                print(f'Ano confirmado: {date}')
+                print("Viagem confirmada!")
+
+            else:
+                print("Hora invalida")
+
+        else:
+            print('Coordenadas invalidas')
     else:
         print(f'Energia de {ener}%')
         print("Energia insulficiente")
-        return False
-    
-    if localx == float and localy == float:
-        print(f'Coordenadas detectadas x:{localx} y: {localy}')
-        return True
-    else:
-        print('Coordenadas invalidas')
-        return False
-    
-    if time == datetime.strftime(time, "%H: %M:"):
-        print("Hora confirmada")
-        return True
-    else:
-        print("Hora invalida")
-        return False
-    
-    if ener == True and localx == True and localy == True and time == True:
-        print("Viagem confirmada!")
-
-    else:
-        print("Algo deu errado")
-
+       
 
 if __name__ == '__main__':
 
     print('--- --- Iniciando o programa --- ---')
 
-    viagem_no_tempo(80,8,70,2300)
+    viagem_no_tempo(100,-512,2570,224)
 
 
     print('--- --- Terminando o programa --- ---')
