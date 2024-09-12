00e77bf17d59e5218b566b4c9e090e8a0df7001a
Hélio de Siqueira Figueiredo Filho <heliosiqueiraff@gmail.com>
2024-09-11 23:48:59 -0300

    first question

---
+++ aula05/lista_de_exercicios/questao_1.py
@@ -0,0 +1,15 @@
+def passos(x,y):
+    print(f"quantos passos ao norte: {x}")
+    print(f"quantos passos ao leste: {y}")
+    soma= x+y
+    print(f'total de passos: {soma}')
+
+if __name__ == '__main__':
+
+    print('--- --- Iniciando o programa --- ---')
+
+    passos(8,10)
+    
+
+
+    print('--- --- Terminando o programa --- ---')
